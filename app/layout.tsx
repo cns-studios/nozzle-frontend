ba004e66093e4580772b46b97aa9f98cf6e07453
CNS Studios <contact@cns-studios.com>
2026-08-04 16:04:54 +0200

    resolve next build type errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,13 @@ export default function RootLayout({
         <link
           rel="preconnect"
           href="https://fonts.googleapis.com"
-          crossOrigin="true"
+          crossOrigin="anonymous"
         />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap"
           rel="stylesheet"
